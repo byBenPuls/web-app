407e8e8a553f164687d5586c38b32c3dee0a48b5
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2025-01-06 04:07:54 +0500

    add: example handlers

---
+++ src/handlers.c
@@ -0,0 +1,32 @@
+#include "handlers.h"
+#include "string.h"
+
+void hello_handler(char *response, size_t response_size)
+{
+    const char *html_template =
+        "<!DOCTYPE html>"
+        "<html lang=\"en\">"
+        "<head>"
+        "<meta charset=\"UTF-8\">"
+        "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+        "<title>Hello Page</title>"
+        "</head>"
+        "<body>"
+        "<h1>Hello, World!</h1>"
+        "<p>Welcome to the Hello Page.</p>"
+        "</body>"
+        "</html>";
+
+    snprintf(response, response_size,
+             "HTTP/1.1 200 OK\r\n"
+             "Content-Type: text/html\r\n"
+             "Content-Length: %zu\r\n"
+             "\r\n"
+             "%s",
+             strlen(html_template), html_template);
+}
+
+void goodbye_handler(char *response, size_t response_size)
+{
+    snprintf(response, response_size, "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nGoodbye, World!");
+}
---
+++ src/handlers.h
@@ -0,0 +1,9 @@
+#ifndef HANDLERS_H
+#define HANDLERS_H
+
+#include <stdio.h>
+
+void hello_handler(char *response, size_t response_size);
+void goodbye_handler(char *response, size_t response_size);
+
+#endif // HANDLERS_H
