26e8b60104ec049025f6612232329848199e2bcf
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2025-01-06 04:29:06 +0500

    add: include routers function

--- main.c
+++ main.c
@@ -3,11 +3,19 @@
 #include "src/server.h"
 #include "src/handlers.h"
 
+
+void include_routers(Router *router)
+{
+    add_route(router, "hello", hello_handler);
+    add_route(router, "goodbye", goodbye_handler);
+    add_route(router, "", main_handler);
+}
+
+
 int main()
 {
     Router *router = create_router();
-    add_route(router, "hello", hello_handler);
-    add_route(router, "goodbye", goodbye_handler);
+    include_routers(router);
 
     start_server(8080, router);
 
