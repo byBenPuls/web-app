b3bef7d8d3ef68908d06f195a2bab23bce528fc7
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2025-01-07 01:57:34 +0500

    add: ip address to args

--- src/server.h
+++ src/server.h
@@ -1,8 +1,8 @@
-#ifndef HTTP_SERVER_H
-#define HTTP_SERVER_H
+#ifndef SERVER_H
+#define SERVER_H
 
 #include "router.h"
 
-void start_server(int port, Router *router);
+void start_server(int port, const char *ip, Router *router);
 
-#endif // HTTP_SERVER_H
+#endif // SERVER_H
