96b7bb7e26ebf39822c0ee7278e25cff7e93a847
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2025-01-07 01:56:38 +0500

    add: json handler

--- src/handlers.c
+++ src/handlers.c
@@ -4,21 +4,22 @@
 
 Response hello_handler(Request *request)
 {
+    printf("Request PATH and METHOD: %s %s\n", request->path, request->method);
     return create_response(
         "200 OK",
         "Content-Type: text/html",
         "<!DOCTYPE html>"
         "<html lang=\"en\">"
         "<head><title>Hello Page</title></head>"
-        "<body><h1>Hello, World!</h1><p>Welcome to the Hello Page.</p></body>"
+        "<body><h1>Hello from C language!</h1><p>Welcome to the Hello Page.</p></body>"
         "</html>");
 }
 
 Response goodbye_handler(Request *request)
 {
     return create_response("200 OK",
-    "Content-Type: text/plain",
-    "Goodbye, World!");
+                           "Content-Type: text/plain",
+                           "Goodbye, World!");
 }
 
 Response main_handler(Request *request)
@@ -26,4 +27,12 @@ Response main_handler(Request *request)
     return create_response("200 OK",
                            "Content-Type: text/plain",
                            "Welcome to the Main Page!");
+}
+
+Response json_handler(Request *request)
+{
+    return create_json_response(
+        "200 OK",
+        "", //
+        "{\"key\":\"value\", \"hello\":\"world\"}");
 }
--- src/handlers.h
+++ src/handlers.h
@@ -7,5 +7,6 @@
 Response hello_handler(Request *request);
 Response goodbye_handler(Request *request);
 Response main_handler(Request *request);
+Response json_handler(Request *request);
 
 #endif // HANDLERS_H
