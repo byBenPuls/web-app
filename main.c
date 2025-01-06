5c27701f09c1f7633a4648eea7adc714dba27a9b
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2025-01-07 01:56:28 +0500

    add: host and json handler

--- main.c
+++ main.c
@@ -4,21 +4,22 @@
 #include "src/handlers.h"
 
 const int APP_PORT = 8080;
+const char *APP_HOST = "0.0.0.0";
 
 void include_routers(Router *router)
 {
     add_route(router, "*", "/hello", hello_handler);
     add_route(router, "*", "/goodbye", goodbye_handler);
     add_route(router, "*", "/", main_handler);
+    add_route(router, "GET", "/json", json_handler);
 }
-
 
 int main()
 {
     Router *router = create_router();
     include_routers(router);
 
-    start_server(APP_PORT, router);
+    start_server(APP_PORT, APP_HOST, router);
 
     free_router(router);
     return 0;
