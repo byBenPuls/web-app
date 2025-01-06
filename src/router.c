4af95f17b326a4737e07faffaf095c71b55b4627
Ben Puls <61308126+byBenPuls@users.noreply.github.com>
2025-01-07 01:57:02 +0500

    add: work with path

--- src/router.c
+++ src/router.c
@@ -1,11 +1,43 @@
 #include "router.h"
 #include "http.h"
+#include "string.h"
 
 Router *create_router()
 {
     Router *router = (Router *)malloc(sizeof(Router));
     router->head = NULL;
     return router;
+}
+
+void extract_path(const char *url, char *path, size_t path_size)
+{
+    const char *question_mark = strchr(url, '?');
+
+    if (question_mark != NULL)
+    {
+        size_t length = question_mark - url;
+        if (length < path_size)
+        {
+            strncpy(path, url, length);
+            path[length] = '\0';
+        }
+        else
+        {
+            strncpy(path, url, path_size - 1);
+            path[path_size - 1] = '\0';
+        }
+    }
+    else
+    {
+        strncpy(path, url, path_size - 1);
+        path[path_size - 1] = '\0';
+    }
+
+    size_t path_length = strlen(path);
+    if (path_length > 1 && path[path_length - 1] == '/')
+    {
+        path[path_length - 1] = '\0';
+    }
 }
 
 void add_route(Router *router, const char *method, const char *path, Handler handler)
@@ -23,24 +55,26 @@ Response handle_request(Router *router, Request *request)
     Route *current = router->head;
     while (current != NULL)
     {
-        if (strcmp(current->path, request->path) == 0)
+        const char *url = request->path;
+        char path[256];
+        extract_path(url, path, sizeof(path));
+        printf("curr path: %s path: %s\n", current->path, path);
+        if (strcmp(current->path, path) == 0)
         {
             if (strcmp(current->method, request->method) == 0 || strcmp(current->method, "*") == 0)
             {
-                return current->handler(request); // Передаем буфер ответа
+                return current->handler(request);
             }
             return create_response(
                 "405 Method Not Allowed",
                 "Content-Type: text/plain",
-                "⚠️⚠️⚠️⚠️ не тот метод ты выбрал.."
-            );
+                "Method Not Allowed");
         }
         current = current->next;
     }
     return create_response("404 Not Found",
-    "Content-Type: text/plain",
-    "404 Not Found"
-    );
+                           "Content-Type: text/plain",
+                           "404 Not Found");
 }
 
 void free_router(Router *router)
